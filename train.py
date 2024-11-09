58bb4ae380f14e169eccbe58fe95bc06094deeb2
Vishwa44 <vishwa.gohil11@gmail.com>
2024-11-08 20:42:36 -0500

    weather

--- train.py
+++ train.py
@@ -267,6 +267,85 @@ class config_ettm1_patchtst():
         self.train_scale = 1.0
         pass
 
+class config_weather_patchtst():
+    def __init__(self, name="", seq_len=512, pred_len=96, num_epochs=1) -> None:
+        self.model_type = "PatchTST"
+        self.is_training = 1
+        self.model_id = "PatchTST_attn_Etth1_"+str(seq_len)+"_"+str(pred_len)+"_"+name
+        self.model = "PatchTST"
+        self.data = "custom"
+        self.root_path = r"D:\Coursework\MTS\dataset"
+        self.data_path = "weather.csv"
+        self.features = "M"
+        self.target = "OT"
+        self.freq = "h"
+        self.checkpoints = "./checkpoints/"
+        self.seq_len = seq_len
+        self.label_len = 48
+        self.pred_len = pred_len
+        self.fc_dropout = 0.2
+        self.head_dropout = 0.0
+        self.patch_len = 16
+        self.stride = 8
+        self.padding_patch = "end"
+        self.affine = 0
+        self.subtract_last = 0
+        self.decomposition = 0
+        self.kernel_size = 25
+        self.individual = 0
+        self.embed_type = 0
+        self.enc_in = 21
+        self.dec_in = 21
+        self.c_out = 7
+        self.d_model = 128
+        self.predictor_d_model = 128
+        self.revin = 1
+        self.n_heads = 16
+        self.e_layers = 3
+        self.d_layers = 1
+        self.d_ff = 256
+        self.moving_avg = 25
+        self.factor = 1
+        self.distil = True
+        self.dropout = 0.2 # 0.2
+        self.fusion_dropout = 0.3
+        self.proj_dropout = 0.3
+        self.embed = "timeF"
+        self.activation = "gelu"
+        self.output_attention = False
+        self.do_predict = False
+        self.num_workers = 2
+        self.itr = 1
+        self.train_epochs = num_epochs
+        self.batch_size = 128
+        self.patience = 50
+        self.learning_rate = 0.0001
+        self.des = "Exp"
+        self.loss = "mse"
+        self.lradj = "type3"
+        self.pct_start = 0.3
+        self.use_amp = False
+        self.use_gpu = True
+        self.gpu = 0
+        self.use_multi_gpu = False
+        self.devices = '0,1,2,3'
+        self.test_flop = False
+        self.profile = False
+        self.scheduler = True
+        self.use_norm = True
+        self.embedding_model = True
+        # jepa
+        self.enc_mask_scale=(0.85, 1)
+        self.pred_mask_scale=(0.15, 0.2)
+        self.use_embed = True
+        self.nenc=1
+        self.npred=3
+        self.allow_overlap=False
+        self.min_keep=5
+        self.embedding_model = True
+        self.ema = [0.996, 1.0]
+        self.train_scale = 1.0
+        pass
 
 def _get_data(args, flag, collator=None):
         data_set, data_loader = data_provider(args, flag, collator)
